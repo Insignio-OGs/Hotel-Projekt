113612c4266331a7265c4ed75359ab0bfd79cab8
max.gerlach <max.gerlach@insignio.de>
2022-09-30 09:24:58 +0200

    schul-31: added a funktion to get all column names from a db table

--- source/DBHandler.php
+++ source/DBHandler.php
@@ -19,4 +19,22 @@ class DBHandler {
         $conf = $utils->getFullConfig();
         $this->conn = mysqli_connect($conf['db_host'],$conf['db_user'],$conf['db_password'],$conf['db_name']);
     }
+
+    /**
+     * Returns all table names from a table as array
+     * @param $tableName
+     * @return array|false
+     */
+    function getColumnNames($tableName) {
+        $res = mysqli_query($this->conn, "SHOW COLUMNS FROM " . $tableName);
+        if(!$res) {
+            return false;
+        }
+        while($cRow = mysqli_fetch_assoc($res)){
+            $column_name[] = $cRow["Field"];
+        }
+        return $column_name;
+    }
+
+
 }
