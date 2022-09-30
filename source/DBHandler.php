3a8ff0c8b2cc425c4bcb720739ea869394d15ef9
max.gerlach <max.gerlach@insignio.de>
2022-09-30 11:09:21 +0200

    schul-31: adjustments

--- source/DBHandler.php
+++ source/DBHandler.php
@@ -21,20 +21,26 @@ class DBHandler {
     }
 
     /**
-     * Returns all table names from a table as array
+     * Returns all column names from a table as array
      * @param $tableName
      * @return array|false
      */
     function getColumnNames($tableName) {
         try {
-            $res = mysqli_query($this->conn, "SHOW COLUMNS FROM " . $tableName);
+            //TODO: Testen sobald die getTableNames funktion existiert
+            $tableNames = $this->getTableNames();
+            if (!in_array($tableName, $tableNames)) {
+                return false;
+            }
+            $res = $this->conn->query("SHOW COLUMNS FROM " . $tableName);
         } catch (Exception $e) {
             return false;
         }
         if (!$res) {
             return false;
         }
-        while ($cRow = mysqli_fetch_assoc($res)) {
+        $column_name = array();
+        while ($cRow = $res->fetch_assoc()) {
             $column_name[] = $cRow["Field"];
         }
         return $column_name;
