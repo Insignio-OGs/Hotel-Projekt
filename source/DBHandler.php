9e3dac84969836554caca5b9dd5864b97918867f
max.gerlach <max.gerlach@insignio.de>
2022-09-30 10:27:50 +0200

    schul-32: added function to get Primarykey columnname

--- source/DBHandler.php
+++ source/DBHandler.php
@@ -19,4 +19,25 @@ class DBHandler {
         $conf = $utils->getFullConfig();
         $this->conn = mysqli_connect($conf['db_host'],$conf['db_user'],$conf['db_password'],$conf['db_name']);
     }
+
+    /**
+     * get Primarykey Columnname
+     * @param $tableName
+     * @return false|mixed
+     */
+    function getPrimaryKey($tableName) {
+        try {
+            $res = mysqli_query($this->conn, "SHOW INDEX FROM " . $tableName . " WHERE Key_name = 'PRIMARY'");
+        } catch (Exception $e) {
+            return false;
+        }
+
+        if (!$res) {
+            return false;
+        }
+
+        $cRow = mysqli_fetch_assoc($res);
+        $column_name = $cRow["Column_name"];
+        return $column_name;
+    }
 }
