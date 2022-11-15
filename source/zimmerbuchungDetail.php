acae08579d220a6a3ebda497855d9f2aa78f278c
jclaus <jan.claus@insignio.de>
2022-11-15 08:24:53 +0100

    Schul-41-2.0, basic design created

---
+++ source/zimmerbuchungDetail.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zimmerbuchung Detail</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <nav>
+        <ul class="nav-list">
+            <li class="nav-item">
+                <img src="images/iconNav.png" alt="">
+            </li>
+            <li class="nav-item">
+                <a href="../index.php">Startseite</a>
+            </li>
+            <li class="nav-item">
+                <a href="fahrzeugbuchung.php">Fahrzeugbuchung</a>
+            </li>
+            <li class="nav-item">
+                <a href="zimmerbuchung.php">Zimmerbuchung</a>
+            </li>
+            <li class="nav-item">
+                <a href="kontaktseite.php">Kontaktseite</a>
+            </li>
+            <li class="nav-item">
+                <div class="login-container">
+                    <img class="loginLogo" src="images/iconLogin.png" alt="">
+                    <a class="loginText" href="login.php"  name="submit">Login</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    <section>
+        <div class="zimmerbuchungDetail">
+            <div class="zimmberbuchungImgGallery">
+                <div class="zimmberbuchungContent1">
+                    <img src="images/doppelzimmer standard.jpg" alt="">
+                </div>
+                <div class="zimmberbuchungContent2">
+                    <img src="images/doppelzimmer komfort.jpg" alt="">
+                    <img src="images/einzelzimmer.jpg" alt="">
+                    <img src="images/familienzimmer.jpg" alt="">
+                </div>
+            </div>
+            <div class="zimmberbuchungInfo1">
+                <span>5.0</span>
+                <span>Sehr gut</span>
+                <span class="stars">
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                    <i class='bx bxs-star'></i>
+                </span>
+                <span class="share">
+                    <i class='bx bx-share-alt'></i>
+                </span>
+            </div>
+            <div class="zimmberbuchungInfo2">
+                <h1>B&B Hotel Kassel-City</h1>
+                <h4>Waldauer Fußweg 3, 34123 Kassel•0561 574490</h4>
+                <h5>Beschreibung</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
+                    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
+                    sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
+                    elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
+                    kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+                    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
+                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+                    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+            <div class="zimmberbuchungInfo3">
+                <h5>Hotel features</h3>
+                <li><i class="bx bx-wifi"><span>Wi-Fi</span></i></li>
+                <li><i class="bx bx-bed"><span>Bed</span></i></li>
+                <li><i class="bx bxs-baguette"><span>Breakfast</span></i></li>
+                <li><i class="bx bx-tv"><span>TV</span></i></li>
+            </div>
+            <div class="booking-btn">
+                <button>Jetzt buchen</button>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-content">
+                <h1 class="footer-h1">Das ist der Footer</h1>
+                <a href="impressum.php">Impressum</a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
