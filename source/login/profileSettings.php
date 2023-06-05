ef5ab1fc42104cf68ffbd57cbd46a00d9ec1d147
Jans dicke Hundemutter <michael.ruemmler@googlemail.com>
2023-06-05 17:00:35 +0200

    ಠ_ಠ

--- source/login/profileSettings.php
+++ source/login/profileSettings.php
@@ -1,3 +1,15 @@
+<?php
+if(!isset($_SESSION)){ 
+    session_start();
+}
+require_once('../include.php');
+$db = new DBHandler();
+$userinfo = $db->getUserInfo(); 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->setUserInfo($_POST);
+  }
+
+?>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
@@ -5,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
     <link href="../../dist/main.css" type="text/css" rel="stylesheet">
-    <title>Document</title>
+    <title>Profileinstellungen</title>
 </head>
 <body>
     <section class="app">
@@ -32,9 +44,46 @@
             </ul>
             </nav>
         </aside>
-        <div class="dashboardContent">
-                
-        </div>
+        <div>
+  <h3>Profileinstellungen</h3>
+  <form method="POST" action="profileSettings.php">
+  <ul>
+    <li>
+      <label for="username">Benutzername:</label>
+      <input type="text" id="username" name="username" value="<?php echo $userinfo[0][1]; ?>">
+
+    </li>
+    <li>
+      <label for="first_name">Vorname:</label>
+      <input type="text" id="first_name" name="first_name" value="<?php echo $userinfo[0][4]; ?>">
+
+    </li>
+    <li>
+      <label for="last_name">Nachname:</label>
+      <input type="text" id="last_name" name="last_name" value="<?php echo $userinfo[0][5]; ?>">
+
+    </li>
+    <li>
+      <label for="email">E-Mail:</label>
+      <input type="text" id="email" name="email" value="<?php echo $userinfo[0][6]; ?>">
+
+    </li>
+    <li>
+      <label for="password">Passwort:</label>
+      <input type="password" id="password" name="password">
+    </li>
+    <li>
+      <label for="password">Passwort erneut eingeben</label>
+      <input type="password" id="passwordcheck" name="passwordcheck">
+    </li>
+    <li>
+      <label for="password">Neues Passwort</label>
+      <input type="password" id="passwordnew" name="passwordnew">
+      <button type="submit" name="Speichern" value="Speichern">Speichern</button>
+    </li>
+  </ul>
+</form>
+</div>
     </section>    
 </body>
 </html>
