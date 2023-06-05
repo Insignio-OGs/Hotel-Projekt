4076e0177a4560829fbc9248518e976132e89ad5
jclaus <jan.claus@insignio.de>
2023-06-05 20:50:16 +0200

    Adjustments

--- source/login/dashboard-nav.php
+++ source/login/dashboard-nav.php
@@ -4,7 +4,7 @@
             <a href="profileSettings.php"><i class="bx bx-user-circle"></i> <span>Profileinstellungen</span></a>
             <ul class="nav-flyout">
                 <li>
-                    <a href="Buchungen.php"><i class="bx bxs-bar-chart-alt-2"></i>Inaktive Buchungen</a>
+                    <a href="Buchungen.php"><i class="bx bxs-bar-chart-alt-2"></i>Buchungen</a>
                 </li>
                 <li>
                     <a href='../../index.php'><i class="bx bx-home-alt icon"></i>Home</a>
